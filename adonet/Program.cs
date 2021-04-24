1e6730f1f0da8277dd3f357465093f53cfd8babc
klykastik2000@gmail.com <klykastik2000@gmail.com>
2021-04-25 03:01:54 +0600

    first task

--- adonet/Program.cs
+++ adonet/Program.cs
@@ -10,16 +10,35 @@ namespace adonet
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void contracts()
         {
             string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-
+            string sql = "SELECT iden.IIN, con.FromDate, con.Type, ib.Money FROM [Identity] as iden JOIN Clients as cl ON cl.IDIdentity = iden.ID JOIN Contracts as con ON cl.IDContract = con.ID JOIN IBAN as ib ON con.IDIBAN = ib.ID; ";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                Console.WriteLine("Подключение открыто");
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3));
+
+                    while (reader.Read())
+                    {
+                        object iin = reader.GetValue(0);
+                        object fd = reader.GetValue(1);
+                        object type = reader.GetValue(2);
+                        object m = reader.GetValue(3);
+
+                        Console.WriteLine("{0} \t{1} \t{2} \t{3}", iin, fd, type, m);
+                    }
+                }
             }
-            Console.WriteLine("Подключение закрыто...");
+        }
+        static void Main(string[] args)
+        {
+            contracts();
+            Console.ReadKey();
         }
     }
 }
