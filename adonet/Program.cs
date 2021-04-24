19a432a406e3875ebca4d19d25ceab64690daa39
klykastik2000@gmail.com <klykastik2000@gmail.com>
2021-04-25 02:22:36 +0600

    Добавьте файлы проекта.

---
+++ adonet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Подключение открыто");
+            }
+            Console.WriteLine("Подключение закрыто...");
+        }
+    }
+}
